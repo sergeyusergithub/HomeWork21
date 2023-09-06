243516908473bbac44585ff1bb73f36a6ad3a1d1
Sergey Sergey <sergeymailnew@mail.ru>
2023-09-06 20:05:59 +0500

    Add project files.

---
+++ HomeWork21/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int x,y;
+
+	// Задача. Поменять значение переменных с помощью указателей на них
+	std::cout << "Задача 1.\n";
+	std::cout << "Введите х -> ";
+	std::cin >> x;
+	std::cout << "Введите y -> ";
+	std::cin >> y;
+
+	int* px, * py;
+
+	px = &x;
+	py = &y;
+
+	std::swap(*px, *py);
+
+	std::cout << "\nх = " << x << "\ny = " << y << "\n\n";
+
+
+
+
+
+
+
+	return 0;
+}
